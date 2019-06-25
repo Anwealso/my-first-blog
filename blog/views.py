dad9cb3f751c2bdb8996af10962628f79102e038
Anwealso <alnicholo@gmail.com>
2019-06-25 12:17:06 +1000

    Updated html template, Template now linked to model. Posts displayed on root page

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from django.utils import timezone
+#Include the model we have written in models.py
+#The dot before models means current directory or current application. Both
+# views.py and models.py are in the same directory. This means we can use . and
+# the name of the file.
+from .models import Post
 
 # Create your views here.
 
@@ -7,4 +13,5 @@ def post_list(request):
     A function that takes request and will return the value it gets from calling
     another function render that will render (put together) our template
     blog/post_list.html."""
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts' : posts})
